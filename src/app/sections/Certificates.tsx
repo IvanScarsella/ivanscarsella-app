920c440787feacabd313b95c986e8e37f59ef9c4
Iván Scarsella <scarsellaivan@gmail.com>
2024-04-09 15:44:07 -0300

    certificates update

--- src/app/sections/Certificates.tsx
+++ src/app/sections/Certificates.tsx
@@ -4,6 +4,7 @@ import imagen2 from '../../../public/certificates/Profesorado.png'
 import imagen3 from '../../../public/certificates/Henry.png'
 import imagen4 from '../../../public/certificates/Certificado Responsive Web Design - FreeCodeCamp.png'
 import imagen5 from '../../../public/certificates/JavaScript Algorithms and Data Structures - FreeCodeCamp.png'
+import imagen6 from '../../../public/certificates/Data Visualization - FreeCodeCamp.png'
 
 const Certificates = () => {
   const images = [
@@ -15,18 +16,28 @@ const Certificates = () => {
       src: imagen2,
       alt: 'Profesorado en Música,\n orientación: Música Popular',
     },
-    { src: imagen3, alt: 'Fullstack Developer\n Henry' },
-    { src: imagen4, alt: 'Responsive Web Design\n FreeCodeCamp' },
+    {
+      src: imagen3,
+      alt: 'Fullstack Developer\n Henry'
+    },
+    {
+      src: imagen4,
+      alt: 'Responsive Web Design\n FreeCodeCamp'
+    },
     {
       src: imagen5,
       alt: 'JavaScript Algorithms and Data Structures\n FreeCodeCamp',
+    },
+    {
+      src: imagen6,
+      alt: 'Data Visualization\n FreeCodeCamp',
     },
   ]
 
   const containerHeight = 164 // Altura máxima del contenedor
 
   return (
-    <div className='m-8 mx-28 max-sm:mx-0'>
+    <div className='m-8 mx-10 max-sm:mx-0'>
       <div className='flex flex-row flex-wrap items-center justify-around max-sm:flex-col'>
         {[...images].map((image, index) => {
           const imageHeight = (containerHeight / 3) * 2
@@ -34,7 +45,7 @@ const Certificates = () => {
           return (
             <div
               key={index}
-              className='mb-8 flex w-1/2 flex-shrink-0 flex-col  items-center hover:scale-150 md:w-1/3 lg:w-1/4 xl:w-1/5'
+              className='mb-8 flex w-1/2 flex-shrink-0 flex-col  items-center hover:scale-150 md:w-1/3 lg:w-1/4 xl:w-1/6'
             >
               <Image
                 src={image.src}
