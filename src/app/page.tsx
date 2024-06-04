c42c68ec18dc492a2ff9f23f4d5898a94961c76c
Iván Scarsella <scarsellaivan@gmail.com>
2024-06-04 14:52:50 -0300

    fix responsive to landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,25 +16,25 @@ export default function Home() {
   const [isHovered, setIsHovered] = useState(false);
   return (
     <main className='relative flex h-full w-full flex-row items-center'>
-      <h1 className="absolute top-[70px] left-0 right-0 flex items-center justify-center font-outfit text-[64px]">Iván Scarsella</h1>
-      <div className={`music-background-default flex flex-col items-center ${isHovered ? 'hover:bg-music' : ''}`}>
+      <h1 className="absolute top-[70px] left-0 right-0 flex items-center justify-center font-outfit text-[64px] max-sm:text-3xl">Iván Scarsella</h1>
+      <div className={`music-background-default w-1/2 flex flex-col items-center ${isHovered ? 'hover:bg-music' : ''}`}>
         <div
-          className='h-[330px] w-[330px] rounded-full border border-[#BFBBBB] flex flex-row items-center my-auto cursor-pointer'
+          className='h-[330px] w-[330px] max-md:h-[220px] max-md:w-[220px] max-sm:h-[110px] max-sm:w-[110px] rounded-full border border-[#BFBBBB] flex flex-row items-center my-auto cursor-pointer'
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
           onClick={() => router.push('/music')}
         >
-          <p className='font-megrim text-[64px] mx-auto'>Música</p>
+          <p className='font-megrim text-[64px] max-md:text-[42px] max-sm:text-2xl mx-auto'>Música</p>
         </div>
       </div>
-      <div className={`music-background-default flex flex-col items-center ${isHovered ? 'hover:bg-programming' : ''}`}>
+      <div className={`music-background-default w-1/2 flex flex-col items-center ${isHovered ? 'hover:bg-programming' : ''}`}>
         <div
-          className='h-[330px] w-[330px] rounded-full border border-[#BFBBBB] flex flex-row items-center my-auto cursor-pointer'
+          className='h-[330px] w-[330px] max-md:h-[220px] max-md:w-[220px] max-sm:h-[110px] max-sm:w-[110px] rounded-full border border-[#BFBBBB] flex flex-row items-center my-auto cursor-pointer'
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
           onClick={() => router.push('/programming')}
         >
-          <p className='font-mina text-[45px] mx-auto'>Programación</p>
+          <p className='font-mina text-[45px] max-md:text-[30px] max-sm:text-sm mx-auto'>Programación</p>
         </div>
       </div>
     </main>
